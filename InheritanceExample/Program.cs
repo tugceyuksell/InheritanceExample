69e5dde11bce599bad1805e01967184c3e0ff478
tyuksel <tyuksel@ozatashipyard.com>
2022-07-13 09:59:05 +0300

    Add project files.

---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    class Sekil//base class
+    {
+        protected int genislik;
+        protected int yukseklik;
+        public void genislikAyarla(int g)
+        {
+            genislik = g;
+        }
+        public void yukseklikAyarla(int y)
+        {
+            yukseklik = y;
+        }
+    }
+    class Dikdortgen : Sekil
+    {
+        public int AlanHesapla()
+        {
+            return (genislik * yukseklik);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.genislikAyarla(4);
+            dikdortgen.yukseklikAyarla(6);
+
+
+            Console.WriteLine("Alan : {0}", dikdortgen.AlanHesapla());
+
+            Console.ReadKey();
+        }
+    }
+    
+}
